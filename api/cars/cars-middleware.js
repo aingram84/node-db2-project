e498b7d3fb70417e361ddaa7f338311787868242
Adam Ingram <kr0nai84@gmail.com>
2024-05-04 09:34:35 -0700

    initial commit

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,6 +1,17 @@
+const Car = require("./cars-model");
+const db = require("../../data/db-config");
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const err = { satus: 404 };
+    const id = req.params.id;
+  if (id === undefined) {
+    res.status(404).json({
+      message: `car with id ${id} is not found`,
+    });
+  } else {
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
